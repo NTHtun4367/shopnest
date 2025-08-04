756723a60f353f59a967fe6bb5671b66f426ac6c
NTHtun4367 <nay4135123@gmail.com>
2025-08-04 15:47:23 +0630

    express-validator

--- server/src/routes/product.ts
+++ server/src/routes/product.ts
@@ -5,10 +5,19 @@ import {
   updateProduct,
 } from "../controllers/product";
 import { isAdmin, protect } from "../middlewares/authMiddleware";
+import { createProductValidator } from "../validators/product";
+import { validateRequest } from "../middlewares/validateRequest";
 
 const router = Router();
 
-router.post("/products", protect, isAdmin, createProduct);
+router.post(
+  "/products",
+  protect,
+  isAdmin,
+  createProductValidator,
+  validateRequest,
+  createProduct
+);
 router.put("/products/:id", protect, isAdmin, updateProduct);
 router.delete("/products/:id", protect, isAdmin, deleteProduct);
 
